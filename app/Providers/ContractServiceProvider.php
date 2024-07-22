4556025f33dc236ad2c775f566571f2dd8a80579
Hugo Pozos <pozos_herrera@outlook.es>
2024-07-22 16:53:38 -0600

    first commit

---
+++ app/Providers/ContractServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContractServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        /**
+         * Bind repositories
+         */
+
+        /**
+         * Bind services
+         */
+
+
+    }
+}
